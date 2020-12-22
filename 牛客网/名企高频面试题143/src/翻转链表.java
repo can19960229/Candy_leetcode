8feba7290b41f65ff397a8f370e1a258664b8ccc
can19960229 <1361050253@qq.com>
2020-12-22 20:56:49 +0800

    翻转链表

---
+++ 牛客网/名企高频面试题143/src/翻转链表.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (C), 2019-2020
+ * author  candy_chen
+ * date   2020/12/22 20:33
+ * version 1.0
+ * Description: 单链表的翻转
+ */
+
+/**
+ *
+ */
+public class 翻转链表 {
+    public ListNode ReverseList(ListNode head){
+        if (head ==null || head.next==null)
+            return head;
+        //初始化pre指针，用于记录当前结点的前一个结点地址
+        ListNode pre = null;
+        //初始化p指针，用于记录当前结点的下一个结点地址
+        ListNode p = null;
+        //head指向null时，循环终止。
+        while(head != null){
+            //先用p指针记录当前结点的下一个结点地址。
+            p = head.next;
+            //让被当前结点与链表断开并指向前一个结点pre。
+            head.next = pre;
+            //pre指针指向当前结点
+            pre = head;
+            //head指向p(保存着原链表中head的下一个结点地址)
+            head = p;
+        }
+        return pre;//当循环结束时,pre所指的就是反转链表的头结点
+    }
+
+}
