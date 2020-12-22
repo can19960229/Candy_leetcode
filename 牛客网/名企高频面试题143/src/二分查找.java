d14b06b621eda3a03a9549d1e8a80dc6252f77f5
can19960229 <1361050253@qq.com>
2020-12-22 20:56:07 +0800

    二分查找

---
+++ 牛客网/名企高频面试题143/src/二分查找.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C), 2019-2020
+ * author  candy_chen
+ * date   2020/12/22 20:29
+ * version 1.0
+ * Description: 二分查找的元素返回的是位置的值
+ */
+
+/**
+ *
+ */
+public class 二分查找 {
+    /**
+     * 二分查找
+     * @param n int整型 数组长度
+     * @param v int整型 查找值
+     * @param a int整型一维数组 有序数组
+     * @return int整型
+     */
+    public int upper_bound_ (int n, int v, int[] a) {
+        // write code here
+        int i = 0;
+        int j = n - 1;
+        int mid = i + (j - 1)/ 2;
+        while(i <= j){
+            if(a[mid] < v){
+                i = mid + 1;
+                mid = (i + j)/2;
+            }else if(mid > 0 && a[mid - 1] >= v){
+                j = mid - 1;
+                mid = (i + j)/2;
+            }else{
+                return mid + 1;
+            }
+
+        }
+        return n + 1;
+    }
+}
