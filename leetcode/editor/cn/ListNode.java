3ea16e58db9278dc677585cd49fff58e83a4e2b9
can19960229 <1361050253@qq.com>
2020-12-22 11:33:04 +0800

    测试

---
+++ leetcode/editor/cn/ListNode.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C), 2019-2020
+ * author  candy_chen
+ * date   2020/12/22 11:20
+ * version 1.0
+ * Description: 测试
+ */
+
+/**
+ *
+ */
+public class ListNode {
+
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+
+}
