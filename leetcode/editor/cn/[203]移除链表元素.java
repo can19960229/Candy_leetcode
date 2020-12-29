6803f86db56ec4f7f3ed3e1864c161ef3b9b5005
can19960229 <1361050253@qq.com>
2020-12-29 22:06:43 +0800

    删除链表节点

---
+++ leetcode/editor/cn/[203]移除链表元素.java
@@ -0,0 +1,41 @@
+//删除链表中等于给定值 val 的所有节点。 
+//
+// 示例: 
+//
+// 输入: 1->2->6->3->4->5->6, val = 6
+//输出: 1->2->3->4->5
+// 
+// Related Topics 链表 
+// 👍 504 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution_203 {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null){
+            return null;
+        }
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+
+        ListNode prev = pre,curr = head;
+        while(curr != null){
+            if(curr.val == val){
+                prev.next = curr.next;
+            }else{
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return pre.next;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
