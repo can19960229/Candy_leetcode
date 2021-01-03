b2b1d3b89b84371f0063ff6d4ff22917f6e23b18
can19960229 <1361050253@qq.com>
2021-01-03 21:40:56 +0800

    复制链表

--- leetcode/editor/cn/[590]N叉树的后序遍历.java
+++ leetcode/editor/cn/[590]N叉树的后序遍历.java
@@ -63,13 +63,18 @@ class Solution_590 {
 class Node {
     public int val;
     public List<Node> children;
+    public Node next;
+    public Node random;
+
     public Node() {}
+
     public Node(int _val) {
         val = _val;
     }
     public Node(int _val, List<Node> _children) {
         val = _val;
         children = _children;
+
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
