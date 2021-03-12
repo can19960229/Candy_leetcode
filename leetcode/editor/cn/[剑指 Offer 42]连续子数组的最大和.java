2db031244cbfab6da4a7a737fcd2370a50bbcd40
can19960229 <1361050253@qq.com>
2021-03-12 23:13:11 +0800

    动态规划

--- leetcode/editor/cn/[剑指 Offer 42]连续子数组的最大和.java
+++ leetcode/editor/cn/[剑指 Offer 42]连续子数组的最大和.java
@@ -23,12 +23,13 @@
 //
 // 
 // Related Topics 分治算法 动态规划 
-// 👍 199 👎 0
+// 👍 218 👎 0
 
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution_42_1 {
-    public int maxSubArray(int[] nums) {
+    //遍历
+    public int maxSubArray_1(int[] nums) {
         int ans = nums[0];
         int sum = 0;
         for (int num : nums) {
@@ -41,5 +42,41 @@ class Solution_42_1 {
         }
         return ans;
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)o
+
+    //动态规划
+    public int maxSubArray_2(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+        int[] dp = new int[n];
+        //第一个元素前面没有子数组
+        dp[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(nums[i],nums[i] +dp[i - 1]);
+        }
+        //得到nums的最大子数组
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res,dp[i]);
+        }
+        return res;
+    }
+
+    //动态规划优化
+    public int maxSubArray(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return 0;
+        // base case
+        int dp_0 = nums[0];
+        int dp_1 = 0,res = dp_0;
+        for (int i = 1; i < n; i++) {
+            dp_1 = Math.max(nums[i],nums[i] +dp_0);
+            dp_0 = dp_1;
+            res = Math.max(res,dp_0);
+        }
+        return res;
+    }
+
+
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
